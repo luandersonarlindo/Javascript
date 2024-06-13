8877fad5fe6788d8c74e6f9b446f038b500a3b83
luandersonarlindo <luan.arlindo20166@gmail.com>
2024-06-13 10:22:59 -0300

    Repositorio de JAVASCRIPT

---
+++ Modulo D/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 30
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) { //Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ Modulo E/aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c} \n`)
+    c++
+}
+
+var b = 6 
+do {
+    console.log(`Passo ${b} \n`)
+    b++
+} while (b <= 10);
